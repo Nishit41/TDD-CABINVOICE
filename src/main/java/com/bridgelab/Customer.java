3cc9b00d9944fd30f4f54b5a6ce415e8769f0b16
Nishit <nishitranjan41@gmail.com>
2022-09-27 16:50:23 +0530

    this is customer commit

--- src/main/java/com/bridgelab/Customer.java
+++ src/main/java/com/bridgelab/Customer.java
@@ -1,2 +1,20 @@
-package com.bridgelab;public class Customer {
+package com.bridgelab;
+
+public class Customer {
+    private int id;
+    Ride[] rideList;
+
+
+    public Customer(int id, Ride[] rideList) {
+        this.setId(id);
+        this.rideList = rideList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
